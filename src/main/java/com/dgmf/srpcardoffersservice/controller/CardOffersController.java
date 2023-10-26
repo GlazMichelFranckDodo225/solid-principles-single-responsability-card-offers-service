46e3af9d4003f0b73a286e9d015b8e290a927856
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-27 00:30:10 +0200

    Example of violation of the Single Responsibility Principle (SRP)

--- src/main/java/com/dgmf/srpcardoffersservice/controller/CardOffersController.java
+++ src/main/java/com/dgmf/srpcardoffersservice/controller/CardOffersController.java
@@ -1,7 +1,36 @@
 package com.dgmf.srpcardoffersservice.controller;
 
+import com.dgmf.srpcardoffersservice.model.CardOffersRequest;
+import com.dgmf.srpcardoffersservice.model.CardOffersResponse;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CardOffersController {
+    public CardOffersResponse getOffers(CardOffersRequest request) {
+        // 1 - Get the Request from the Client
+        // 2 - Validate the Request
+        if(request == null) {
+            // Handle exception
+        }
+
+        if(request.getCardNum() == null) {
+            // Handle exception
+        }
+
+        // 3 - Write the Business logic, like :
+        // - Card offers expired
+        // - Get the list of Cards
+        // - Apply the logic to get valid offers
+        // - etc...
+
+        // 4 - Communicate with Backend Systems, like :
+        // - Database
+        // - Service
+        // - File
+        // - etc...
+
+        // 5 - Response logics
+
+        return null;
+    }
 }
--- src/main/java/com/dgmf/srpcardoffersservice/model/CardOffersRequest.java
+++ src/main/java/com/dgmf/srpcardoffersservice/model/CardOffersRequest.java
@@ -1,4 +1,11 @@
 package com.dgmf.srpcardoffersservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class CardOffersRequest {
+    private String cardNum;
 }
